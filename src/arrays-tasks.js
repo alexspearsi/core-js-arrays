a306fa2145a856daac5fc00772eb9b6df6abf0ef
Alex <strelchenko.aleksandr@yandex.ru>
2024-12-18 00:25:27 +0200

    solve the task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -601,8 +601,13 @@ function propagateItemsByPositionIndex(/* arr */) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  if (n > 0) {
+    const firstPart = arr.splice(-n);
+    return [...firstPart, ...arr];
+  }
+  const firstPart = arr.splice(0, Math.abs(n));
+  return [...arr, ...firstPart];
 }
 
 /**
