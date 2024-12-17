0944cc6760b409910329384bbe8986c9481b05ee
Alex <strelchenko.aleksandr@yandex.ru>
2024-12-17 13:37:32 +0200

    solve the task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -383,8 +383,10 @@ function flattenArray(nestedArray) {
  *   selectMany([[1, 2], [3, 4], [5, 6]], (x) => x) =>   [ 1, 2, 3, 4, 5, 6 ]
  *   selectMany(['one','two','three'], (x) => x.split('')) =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  const array = arr.flatMap(childrenSelector);
+
+  return array;
 }
 
 /**
