6a7ef411979f3e186f80f17de92a255bcc52e128
Alex <strelchenko.aleksandr@yandex.ru>
2024-12-19 12:33:57 +0200

    solve the task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -440,8 +440,8 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(length) {
+  return Array.from({ length }, (_, index) => index * 2 + 1);
 }
 
 /**
